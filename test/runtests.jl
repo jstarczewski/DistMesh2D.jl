c0e2c7de21784f7670f6d85aeba03b235546af23
jstarczewski <jan.a.starczewski@gmail.com>
2020-12-09 19:51:25 +0100

    Add simple algorithm runtime test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,9 @@ using Test
 using DataFrames
 using Deldir
 using GeometricalPredicates
+using DistMesh2D
 
 include("edgestests.jl")
 include("scalertests.jl")
 include("triangletests.jl")
+include("distmeshtests.jl")
