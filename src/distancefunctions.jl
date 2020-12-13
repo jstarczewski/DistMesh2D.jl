51cc58510b423564f31df61d37d363194c68ee77
jstarczewski <jan.a.starczewski@gmail.com>
2020-12-13 19:57:15 +0100

    Add distance functions extension and logging possibility

--- src/distancefunctions.jl
+++ src/distancefunctions.jl
@@ -16,3 +16,33 @@ function dcircle(
 )::T where {T<:Float64}
     return sqrt((p[1] - xc) .^ 2 + (p[2] - yc) .^ 2) - r
 end
+
+function dunion(
+    p,
+    dfs::Function...,
+)::Float64
+    return min([fun(p) for fun in dfs]...)
+end
+
+function ddiff(
+    p,
+    d1::Function,
+    dfs::Function...
+)::Float64
+    return max(d1(p), [-fun(p) for fun in dfs]...)
+end
+
+function dintersect(
+        p,
+        dfs::Function...
+)::Float64
+    return max([fun(p) for fun in dfs]...)
+end
+
+function huniform(p)::Float64
+    return ones(size(p, 1), 1)[1]
+end
+
+function protate(p,phi)
+    return p*[cos(phi) -sin(phi); sin(phi) cos(phi)]
+end
